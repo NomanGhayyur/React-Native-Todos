3d446e0c85afc7d764f68f633696df1a7fe78209
NomanGhayyur <nomanghayyur96@gmail.com>
2023-06-16 01:10:13 +0500

    devide layout into auth and inside layout

--- App.tsx
+++ App.tsx
@@ -1,19 +1,53 @@
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import List from "./app/screens/List";
 import Details from "./app/screens/Details";
+import Login from "./app/screens/Login";
+import { User, onAuthStateChanged } from "firebase/auth";
+import { FIREBASE_AUTH } from "./firebaseConfig";
 
-const stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
+const InsideStack = createNativeStackNavigator();
+
+function InsideLayout() {
+  return (
+    <InsideStack.Navigator>
+      <InsideStack.Screen name="My Todos" component={List} />
+      <InsideStack.Screen name="Details" component={Details} />
+    </InsideStack.Navigator>
+  );
+}
 
 export default function App() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      console.log(user, "user");
+      setUser(user);
+    });
+  }, []);
+
   return (
     <NavigationContainer>
-      <stack.Navigator>
-        <stack.Screen name="My Todos" component={List} />
-        <stack.Screen name="Details" component={Details} />
-      </stack.Navigator>
+      <Stack.Navigator initialRouteName="Login">
+        {user ? (
+          <Stack.Screen
+            name="Inside"
+            component={InsideLayout}
+            options={{ headerShown: false }}
+          />
+        ) : (
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+        )}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
