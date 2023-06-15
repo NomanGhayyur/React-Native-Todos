bb56dae410f8ef80583a894271872d84fe9a4592
NomanGhayyur <nomanghayyur96@gmail.com>
2023-06-16 01:11:06 +0500

    added details screen

--- app/screens/Details.tsx
+++ app/screens/Details.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { FIREBASE_AUTH } from "../../firebaseConfig";
+import { View, Text, StyleSheet } from "react-native";
 
 const Details = () => {
-  return <div>Details</div>;
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      setUser(user);
+    });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.infoText}>User Details </Text>
+      <Text style={styles.infoText}>Email: {user?.email} </Text>
+      <Text style={styles.infoText}>Verified: {user?.emailVerified} </Text>
+    </View>
+  );
 };
 
 export default Details;
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+  },
+  infoText: {
+    flex: 1,
+    paddingHorizontal: 4,
+  },
+});
