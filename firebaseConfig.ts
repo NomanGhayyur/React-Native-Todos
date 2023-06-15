0f5c329ce108a28d1128575f110963222ddcbd69
NomanGhayyur <nomanghayyur96@gmail.com>
2023-06-15 22:21:07 +0500

    added Google Auth

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -19,4 +19,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
-// export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const GOOGLE_AUTH_PROVIDER = new GoogleAuthProvider();
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
